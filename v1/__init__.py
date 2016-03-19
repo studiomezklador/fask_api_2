eeed342d95de0b0c96c14bc7270e029ce98c9952
mezklador <s.esteve@mezklador.com>
2016-03-19 02:37:06 +0100

    just some thoughts on v1/__init__.py

--- v1/__init__.py
+++ v1/__init__.py
@@ -6,8 +6,18 @@ api = Blueprint('api', __name__)
 from v1.auth import views, errors
 # from v1.auth.models import User
 
+"""
+Here is how to implement a before_request on a Blueprint.
+NOTE: this API must check if it's a browser-based navigation (or not)
+on any single request (or maybe once?).
+"""
+
 
 def before_api():
+    """
+    JUST create a global in Flask App dict containing client's browsing
+    interface.
+    """
     userAgent = request.user_agent
     g.client = {'browser': userAgent.browser,
                 'platform': userAgent.platform,
@@ -20,12 +30,19 @@ api.before_request(before_api)
 
 @api.route('/')
 def index():
+    """
+    if no browser and no platform: it's a CLI request.
+    """
     if g.client['browser'] is None or g.client['platform'] is None:
-        msg = {'message': "hello from API {} -- in CLI Mode".format(versions[0]), 'status': 200}
+        string = "hello from API {} -- in CLI Mode"
+        msg = {'message': string.format(versions[0]), 'status': 200}
         r = Response(json.dumps(msg))
         r.headers['Content-type'] = 'application/json; charset=utf-8'
         return r, 200
 
+    """
+    ELSE: it's obviously on a web browser
+    """
     string = "<h1>hello from API v1 | {} | {} | {} | {}</h1>"
     return string.format(g.client['browser'],
                          g.client['platform'],
