8320f5e89fec6df0ac4c5cae6835dc905e55ac4e
mezklador <s.esteve@mezklador.com>
2016-03-19 02:28:21 +0100

    Some experiments with Client Browsing Detection, working on :5000/v1/

--- v1/__init__.py
+++ v1/__init__.py
@@ -1,16 +1,33 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, g, Response
+from apicore.scandirs import basedir, root, versions, blueprints
+import simplejson as json
 
 api = Blueprint('api', __name__)
 from v1.auth import views, errors
 # from v1.auth.models import User
 
-"""browser = request.user_agent.browser
-version = request.user_agent.version and int(request.user_agent.version.split('.')[0])
-platform = request.user_agent.platform
-uas = request.user_agent.string"""
+
+def before_api():
+    userAgent = request.user_agent
+    g.client = {'browser': userAgent.browser,
+                'platform': userAgent.platform,
+                'version': userAgent.version,
+                'language': userAgent.language}
+
+
+api.before_request(before_api)
+
 
 @api.route('/')
 def index():
-    userAgent = request.user_agent
-    browser = userAgent.browser
-    return "hello from API v1 | {}".format(browser)
+    if g.client['browser'] is None or g.client['platform'] is None:
+        msg = {'message': "hello from API {} -- in CLI Mode".format(versions[0]), 'status': 200}
+        r = Response(json.dumps(msg))
+        r.headers['Content-type'] = 'application/json; charset=utf-8'
+        return r, 200
+
+    string = "<h1>hello from API v1 | {} | {} | {} | {}</h1>"
+    return string.format(g.client['browser'],
+                         g.client['platform'],
+                         g.client['version'],
+                         g.client['language'])
